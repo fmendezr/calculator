1f312cc107c2f99179ccb486e49c072a760cb3f1
Felix Mendez <mendez.felixrn@gmail.com>
2022-09-07 20:20:28 -0400

    Make operand funciton that consolidtes the previous functions

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-const add = (num1, num2) => {
-    return num1 + num2;
-};
-
-const subtract = (num1, num2) => {
-    return num1 - num2;
-};
-
-const multiply = (num1, num2) => {
-    return  num1 * num2;
-};
-
-const divide = (num1, num2) => {
-    return num1 / num2;
+const operate = (operand, num1, num2) => {
+    switch (operand) {
+        case "add":
+            return num1 + num2;
+        case "subtract":
+            return num1 - num2;
+        case "multiply":
+            return num1 * num2;
+        case "divide":
+            return num1 / num2;
+    };
 };
