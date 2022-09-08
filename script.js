37c885c9ae5f44f1c1a8d301172557a13baa5086
Felix Mendez <mendez.felixrn@gmail.com>
2022-09-08 19:06:21 -0400

    Add event listener to all the number buttons in calculator

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const num1 = [];
+const num2 = [];
+const  operator = undefined; 
+
 const operate = (operand, num1, num2) => {
     switch (operand) {
         case "add":
@@ -10,3 +14,16 @@ const operate = (operand, num1, num2) => {
             return num1 / num2;
     };
 };
+
+const numBtns = document.querySelectorAll(".num-btn");
+const multiply = document.getElementById("multiply");
+
+const drugs = () => {
+    numBtns.forEach(element => {
+        element.addEventListener("click", () => {
+        })
+        console.log(element.textContent)
+    })
+}
+
+drugs()
