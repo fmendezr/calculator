b2a7b895ce026098dc2f7252cdd6bfe24555deb8
Felix Mendez <mendez.felixrn@gmail.com>
2022-09-08 21:31:48 -0400

    Create empty event listener for operand buttons

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const num1 = [];
 const num2 = [];
-const  operator = null; 
+let  operator = null; 
 
 const operate = (operand, num1, num2) => {
+
     switch (operand) {
         case "add":
             return num1 + num2;
@@ -15,19 +16,23 @@ const operate = (operand, num1, num2) => {
     };
 };
 
+// Event listener that when number is clicked assigns its value to num1/num2
 const numBtns = document.querySelectorAll(".num-btn");
-const multiply = document.getElementById("multiply");
-
-
 numBtns.forEach(element => {
      element.addEventListener("click", () => {
         if(operator === null){
             num1.push(element.textContent);
         } else {
-            num2.push(element.textContent)
-        }
+            num2.push(element.textContent);
+        };
         console.log(num1)
         console.log(num2)
-        })
-    })
+        });
+    });
 
+// Event listener that when operand is clicked assigs it to operator button
+const operandbuttons = document.querySelectorAll(".operan-btn");
+operandbuttons.forEach(element => {
+    element.addEventListener("click", () => {
+    })
+})
